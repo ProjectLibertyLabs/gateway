a8740861982b6f9420b5f519a91f6b725688acab
Claire Olmstead <olmsteadclaire@gmail.com>
2024-03-28 15:13:00 -0700

    wip

--- services/account/apps/api/test/accounts.controller.e2e-spec.ts
+++ services/account/apps/api/test/accounts.controller.e2e-spec.ts
@@ -5,6 +5,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { ApiModule } from '../src/api.module';
 import { assert } from 'console';
+import request from 'supertest';
 
 describe('Account Controller', () => {
   let app: INestApplication;
@@ -24,25 +25,32 @@ describe('Account Controller', () => {
     await app.init();
   });
 
-  it('(GET) /accounts/:msaId with valid msaId', () => {
-    // const validMsaId = '1234';
-    // expect(validMsaId).toBe('1234');
-    assert(true, 'Test asserted.');
-    // request(app.getHttpServer())
-    //   .get('/accounts/' + validMsaId)
-    //   .expect(200)
-    //   .expect({ status: 200, message: 'Found account' });
+  it('(GET) /accounts/:msaId with valid msaId', async () => {
+    const validMsaId = '1234';
+    expect(validMsaId).toBe('1234');
+    const req = await request(app.getHttpServer())
+      .get('/accounts/' + validMsaId)
+      .expect(200)
+      .expect({ status: 200, message: 'Found account' })
+      .then(async () => {
+        await app.close();
+      });
+    console.debug(req);
   });
 
-  // it('(GET) /accounts/:msaId with invalid msaId', () => {
-  //   const invalidMsaId = '1';
-  //   request(app.getHttpServer())
-  //     .get('/accounts/' + invalidMsaId)
-  //     .expect(401)
-  //     .expect({ status: 401, message: 'Invalid msaId.' });
+  it('(GET) /accounts/:msaId with invalid msaId', async () => {
+    const invalidMsaId = '1234';
+    const req = await request(app.getHttpServer())
+      .get('/accounts/' + invalidMsaId)
+      .expect(401)
+      .expect({ status: 401, message: 'Invalid msaId.' })
+      .then(async () => {
+        await app.close();
+      });
+    console.debug(req);
+  });
+
+  // afterEach(async () => {
+  //   await app.close();
   // });
-
-  afterEach(async () => {
-    await app.close();
-  });
 });

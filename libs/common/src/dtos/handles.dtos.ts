53d1f7541efd020c86d4785ba2cffb849c51436f
Claire Olmstead <olmsteadclaire@gmail.com>
2024-04-01 11:28:04 -0700

    tests passing and validation working

---
+++ libs/common/src/dtos/handles.dtos.ts
@@ -0,0 +1,16 @@
+import { HexString } from '@polkadot/util/types';
+import { IsNotEmpty } from 'class-validator';
+import { AccountWithHandle } from './accounts.dto';
+
+export class HandlesRequest {
+  @IsNotEmpty()
+  pallet: string;
+
+  @IsNotEmpty()
+  ectrinsicName: string;
+
+  @IsNotEmpty()
+  encodedExtrinsic: HexString;
+}
+
+export type HandlesResponse = AccountWithHandle;

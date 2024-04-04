b1e975bf4a1a281eac3e1455624e9d4ea461db09
Claire Olmstead <olmsteadclaire@gmail.com>
2024-04-04 13:07:02 -0700

    wip

--- apps/api/src/services/handles.service.ts
+++ apps/api/src/services/handles.service.ts
@@ -1,18 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
 import { InjectQueue } from '@nestjs/bullmq';
 import { QueueConstants } from '../../../../libs/common/src';
 import { BlockchainService } from '../../../../libs/common/src/blockchain/blockchain.service';
 import { AccountResponse } from '../../../../libs/common/src/dtos/accounts.dto';
-import { HandlesResponse } from '../../../../libs/common/src/dtos/handles.dtos';
+import { HandlesRequest, HandlesResponse } from '../../../../libs/common/src/dtos/handles.dtos';
+import { Queue } from 'bullmq';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class HandlesService {
-  constructor(private blockchainService: BlockchainService) {}
+  private readonly logger: Logger;
 
-  createHandle(handle: string): string {
-    return 'Handle created successfully: ' + handle;
+  constructor(
+    @InjectRedis() private redis: Redis,
+    @InjectQueue(QueueConstants.ACCOUNT_CHANGE_PUBLISH_QUEUE)
+    private accountChangePublishQueue: Queue,
+    private configService: ConfigService,
+    private blockchainService: BlockchainService,
+  ) {
+    this.logger = new Logger(this.constructor.name);
+  }
+
+  async createHandle(createHandlesRequest: HandlesRequest) {
+    // TODO: figure out how we want to handle creating accounts in relation to siwf.
+    const job = await this.accountChangePublishQueue.add('Create Handle', createHandlesRequest);
+    this.logger.debug(JSON.stringify(job));
+    const response = { msaId: 1, handle: 'handle' };
+    return response;
   }
 
   async getHandle(msaId: number): Promise<HandlesResponse> {

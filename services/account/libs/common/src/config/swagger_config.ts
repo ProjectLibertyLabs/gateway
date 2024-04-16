203e1479904a02752f3161eae3affe571a5f83d2
Claire Olmstead <olmsteadclaire@gmail.com>
2024-04-16 10:43:29 -0700

    update the swagger json file and build new docs/index.html

--- services/account/libs/common/src/config/swagger_config.ts
+++ services/account/libs/common/src/config/swagger_config.ts
@@ -21,6 +21,6 @@ export const initSwagger = async (app: INestApplication, apiPath: string) => {
   });
 
   // write swagger.json to disk
-  // fs.writeFileSync('./swagger.json', JSON.stringify(document));
+  fs.writeFileSync('./swagger.json', JSON.stringify(document));
   SwaggerModule.setup(apiPath, app, document);
 };

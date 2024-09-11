a155e32ff66bfd0182dc40c0f41105ca8a8a70bd
Joe Caputo <joseph.caputo@unfinished.com>
2024-09-11 10:03:37 -0400

    fix: account-worker moved typedefs, compiling

--- libs/account-lib/src/types/enums.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-shadow
-export enum TransactionType {
-  CHANGE_HANDLE = 'CHANGE_HANDLE',
-  CREATE_HANDLE = 'CREATE_HANDLE',
-  SIWF_SIGNUP = 'SIWF_SIGNUP',
-  SIWF_SIGNIN = 'SIWF_SIGNIN',
-  ADD_KEY = 'ADD_KEY',
-  ADD_PUBLIC_KEY_AGREEMENT = 'ADD_PUBLIC_KEY_AGREEMENT',
-}

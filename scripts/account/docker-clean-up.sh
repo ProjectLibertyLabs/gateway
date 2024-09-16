cfdf57c7440363dfd9bb1328dff1cbd9f1392bef
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-09-16 12:04:13 -0400

    chore: various fixes to get e2e tests working for account service (#502)
    # Problem
    
    After changing to the mono-repo, e2e tests do not work because the
    scripts that execute them need updates, due to file locations changing.
    
    # Solution
    
    Update the e2e test workflow so that the tests can execute locally.
    
    ## Steps to Verify:
    
    1. Follow the steps in `./developer-docs]account/README.md` and verify
    that the tests execute correctly.
    
    ---------
    
    Co-authored-by: Joe Caputo <joseph.caputo@unfinished.com>

--- scripts/account/docker-clean-up.sh
+++ scripts/account/docker-clean-up.sh
@@ -6,10 +6,13 @@ docker compose down frequency redis account-service-api account-service-worker
 
 # Remove specified volumes
 echo "Removing specified volumes..."
-docker volume rm gateway_redis_data
-docker volume rm gateway_chainstorage
-docker volume rm gateway_account_node_cache
+docker -f volume rm gateway_redis_data
+docker -f volume rm gateway_chainstorage
+docker -f volume rm gateway_account_api_node_cache
+docker -f volume rm gateway_account_worker_node_cache
+docker volume prune -f
 
 # Remove account-service images
 echo "Removing account-service image..."
-docker rmi account-service
+docker rm gateway-gateway-base-1
+docker rmi account-service gateway-dev
--- scripts/account/restart-chain-docker.sh
+++ scripts/account/restart-chain-docker.sh
@@ -2,16 +2,18 @@
 
 # Stop and remove containers, networks
 echo "Stopping and removing containers, networks..."
-docker compose down
+docker compose -f docker-compose.yaml -f docker-compose-e2e.account.yaml --profile e2e down
 
 # Remove specified volumes
 echo "Removing specified volumes..."
-docker volume rm account-service_redis_data
-docker volume rm account-service_chainstorage
+docker volume rm -f gateway_redis_data
+docker volume rm -f gateway_chainstorage
+docker volume rm -f gateway_acccount_api_node_cache
+docker volume rm -f gateway_account_worker_node_cache
 
 # Start specific services in detached mode
-echo "Starting redis and frequency services..."
-docker compose up -d redis frequency
+echo "Starting redis and frequency instant sealing services..."
+docker compose -f docker-compose.yaml -f docker-compose-e2e.account.yaml --profile e2e up -d
 
 # Wait for 15 seconds
 echo "Waiting 15 seconds for Frequency to be ready..."
@@ -19,12 +21,4 @@ sleep 15
 
 # Run make setup
 echo "Running make setup to provision Provider with capacity, etc..."
-make setup
-
-# Start all services in detached mode
-echo "Starting account-service, api and worker apps..."
-docker compose up -d account-service-api account-service-worker
-
-# Start the mock web server
-echo "Please run 'make mock-webhook' in a separate terminal..."
-
+cd apps/account-api/test/setup && npm install && npm run main

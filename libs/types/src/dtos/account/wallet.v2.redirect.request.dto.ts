c27c80bc7489d8c565032af46a38ab23bf94e0c7
Wil Wade <wil.wade@amplica.io>
2024-10-01 19:22:08 +0000

    Use ApiPropertyOptional

--- libs/types/src/dtos/account/wallet.v2.redirect.request.dto.ts
+++ libs/types/src/dtos/account/wallet.v2.redirect.request.dto.ts
@@ -2,7 +2,7 @@ import { EnsureArray } from '#utils/decorators/ensure-array.decorator';
 import { IsCredentialType } from '#utils/decorators/is-credential-type.decorator';
 import { IsSchemaName } from '#utils/decorators/is-schema-name.decorator';
 import { IsSwifV2CallbackUrl } from '#utils/decorators/is-swif-v2-callback-url.decorator';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 
 export class WalletV2RedirectRequestDto {
@@ -14,10 +14,9 @@ export class WalletV2RedirectRequestDto {
   @IsSwifV2CallbackUrl()
   callbackUrl: string;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description:
       'List of credentials using the types: "VerifiedGraphKeyCredential", "VerifiedEmailAddressCredential", and "VerifiedPhoneNumberCredential". Note that Contact related verifiable credentials will be nested into an anyOf request form.',
-    required: false,
     type: [String],
   })
   @IsOptional()
@@ -25,10 +24,9 @@ export class WalletV2RedirectRequestDto {
   @IsCredentialType({ each: true })
   credentials?: string[];
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description:
       'The list of permissions using the Frequency Schema names and versions. Pattern: `<namespace>.<name>@v<version integer>` e.g. `dsnp.broadcast@v2`',
-    required: false,
     type: [String],
   })
   @IsOptional()

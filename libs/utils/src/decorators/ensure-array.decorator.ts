3158af1bbed67332592b295f1f21feb1880ddc99
Wil Wade <wil.wade@amplica.io>
2024-10-01 19:22:17 +0000

    Add some docs for the ensure array

--- libs/utils/src/decorators/ensure-array.decorator.ts
+++ libs/utils/src/decorators/ensure-array.decorator.ts
@@ -1,5 +1,9 @@
 import { Transform } from 'class-transformer';
 
+/**
+ * Transformer to ensure that the value is an array and will make it an array of one value if it isn't one.
+ * Example: GET Query strings will default to a string not an array if you only send one value, this will transform that single value into an array of 1 or an empty array if no value.
+ */
 export const EnsureArray = Transform(({ value }) => {
   if (Array.isArray(value)) return value;
   return value ? [value] : [];

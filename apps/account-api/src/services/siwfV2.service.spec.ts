2bf74a1f383f480a15e97d79889eada9af3f077b
Wil Wade <wil.wade@amplica.io>
2024-10-02 13:25:39 -0400

    SIWF V2 GET /siwf: Get the Redirect URL (#564)
    # Problem
    
    A provider backend using SIWF V2 (also known as SIWA) wants to be able
    to generate Authentication Redirect URLs with dynamic callback URLs
    instead of using a static or pre-generated `signedPayload`.
    
    Closes: #524
    
    # Solution
    
    - Update `SIWF_URL` (aka v1) to the new v1 path
    - Added new `SIWF_V2_URL` optional environment variable (defaults to
    Frequency Access for now)
    - Added new V2 accounts controller and e2e test
    - Added `WalletV2RedirectRequestDto` with decorators and transformations
    to validate the inputs
    - Added Tests for new (complex) decorators.
    - Added new `SiwfV2Service` to handle the processing of the SIWF V2
    related tasks
    - Updated incorrect docs
    - Fixed issue with failing keys.controller test due to mismatched
    versions of Polkadotjs API.
    
    
    ## Steps to Verify:
    
    1. Run the e2e tests
    2. Test it via
    http://localhost:3000/docs/swagger#/v2%2Faccounts/AccountsControllerV2_getRedirectUrl

---
+++ apps/account-api/src/services/siwfV2.service.spec.ts
@@ -0,0 +1,123 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { decodeSignedRequest } from '@projectlibertylabs/siwfv2';
+import { SiwfV2Service } from './siwfV2.service';
+import { IAccountApiConfig } from '#account-api/api.config';
+import { IBlockchainConfig } from '#account-lib/blockchain/blockchain.config';
+import { WalletV2RedirectResponseDto } from '#types/dtos/account/wallet.v2.redirect.response.dto';
+import { BlockchainService } from '#account-lib/blockchain/blockchain.service';
+import { GenerateMockConfigProvider } from '#testlib/utils.config-tests';
+
+const mockBlockchainConfigProvider = GenerateMockConfigProvider<IBlockchainConfig>('blockchain', {
+  capacityLimit: { serviceLimit: { type: 'percentage', value: 80n } },
+  providerId: 1n,
+  providerSeedPhrase: '//Alice',
+  frequencyUrl: new URL('ws://localhost:9944'),
+  isDeployedReadOnly: false,
+});
+
+const mockAccountApiConfigProvider = GenerateMockConfigProvider<IAccountApiConfig>('account-api', {
+  apiBodyJsonLimit: '',
+  apiPort: 0,
+  apiTimeoutMs: 0,
+  frequencyHttpUrl: new URL('http://127.0.0.1:9944'),
+  graphEnvironmentType: 0,
+  siwfUrl: '',
+  siwfV2Url: 'https://www.example.com/siwf',
+});
+
+const exampleCallback = 'https://www.example.com/callback';
+const examplePermissions = [1, 3, 5, 7, 9];
+const exampleCredentials = [
+  'VerifiedGraphKeyCredential',
+  'VerifiedEmailAddressCredential',
+  'VerifiedPhoneNumberCredential',
+];
+
+describe('SiwfV2Service', () => {
+  let siwfV2Service: SiwfV2Service;
+
+  const mockBlockchainService = {
+    getNetworkType: jest.fn(),
+  };
+
+  const mockBlockchainServiceProvider = {
+    provide: BlockchainService,
+    useValue: mockBlockchainService,
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      providers: [
+        SiwfV2Service,
+        mockBlockchainServiceProvider,
+        mockAccountApiConfigProvider,
+        mockBlockchainConfigProvider,
+      ],
+    }).compile();
+
+    siwfV2Service = module.get<SiwfV2Service>(SiwfV2Service);
+  });
+
+  it('should be defined', () => {
+    expect(siwfV2Service).toBeDefined();
+  });
+
+  describe('getRedirectUrl', () => {
+    let result: WalletV2RedirectResponseDto;
+
+    it('Should build the result', async () => {
+      expect.assertions(1);
+      result = await siwfV2Service.getRedirectUrl(exampleCallback, examplePermissions, exampleCredentials);
+      expect(result).toBeDefined();
+    });
+
+    it('Should return a valid signedRequest', () => {
+      // Assert
+      expect(result).toHaveProperty('signedRequest');
+
+      const parsedResult = decodeSignedRequest(result.signedRequest);
+      expect(parsedResult.requestedSignatures.payload.callback).toEqual(exampleCallback);
+    });
+
+    it('Should return the signedRequest with credentials', () => {
+      // Assert
+      expect(result).toHaveProperty('signedRequest');
+
+      const parsedResult = decodeSignedRequest(result.signedRequest);
+      expect(parsedResult.requestedCredentials).toHaveLength(2);
+    });
+
+    it('Should return the signedRequest with permissions', () => {
+      // Assert
+      expect(result).toHaveProperty('signedRequest');
+
+      const parsedResult = decodeSignedRequest(result.signedRequest);
+      expect(parsedResult.requestedSignatures.payload.permissions).toEqual(examplePermissions);
+    });
+
+    it('Should go to the correct redirect url', () => {
+      const resultUrl = new URL(result.redirectUrl);
+
+      expect(resultUrl.pathname).toEqual('/siwf/start');
+      expect(resultUrl.host).toEqual('www.example.com');
+    });
+  });
+
+  describe('swifV2Endpoint', () => {
+    it('Should go to the correct redirect url', async () => {
+      jest
+        .spyOn(mockAccountApiConfigProvider.useValue, 'siwfV2Url', 'get')
+        .mockReturnValueOnce('https://www.frequencyaccess.com/siwa');
+      const result: WalletV2RedirectResponseDto = await siwfV2Service.getRedirectUrl(
+        exampleCallback,
+        examplePermissions,
+        exampleCredentials,
+      );
+      const resultUrl = new URL(result.redirectUrl);
+
+      expect(resultUrl.host).toEqual('www.frequencyaccess.com');
+      expect(resultUrl.pathname).toEqual('/siwa/start');
+    });
+  });
+});
---
+++ libs/types/src/dtos/account/wallet.v2.redirect.request.dto.ts
@@ -0,0 +1,47 @@
+import { EnsureArray } from '#utils/decorators/ensure-array.decorator';
+import { IsCredentialType } from '#utils/decorators/is-credential-type.decorator';
+import { IsSchemaName } from '#utils/decorators/is-schema-name.decorator';
+import { IsSwifV2CallbackUrl } from '#utils/decorators/is-swif-v2-callback-url.decorator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+
+export class WalletV2RedirectRequestDto {
+  @ApiProperty({
+    description: 'The URL that will be called when the authentication process is completed.',
+    required: true,
+    type: String,
+    example: 'http://localhost:3000/login/callback',
+  })
+  @IsSwifV2CallbackUrl()
+  callbackUrl: string;
+
+  @ApiPropertyOptional({
+    description:
+      'List of credentials using the types: "VerifiedGraphKeyCredential", "VerifiedEmailAddressCredential", and "VerifiedPhoneNumberCredential". Note that Contact related verifiable credentials will be nested into an anyOf request form.',
+    type: [String],
+    example: ['VerifiedGraphKeyCredential', 'VerifiedEmailAddressCredential', 'VerifiedPhoneNumberCredential'],
+  })
+  @IsOptional()
+  @EnsureArray
+  @IsCredentialType({ each: true })
+  credentials?: string[];
+
+  @ApiPropertyOptional({
+    description:
+      'The list of permissions using the Frequency Schema names and versions. Pattern: `<namespace>.<name>@v<version integer>` e.g. `dsnp.broadcast@v2`',
+    type: [String],
+    example: [
+      'dsnp.broadcast@v2',
+      'dsnp.private-follows@v1',
+      'dsnp.reply@v2',
+      'dsnp.reaction@v1',
+      'dsnp.tombstone@v2',
+      'dsnp.update@v2',
+      'frequency.default-token-address@v1',
+    ],
+  })
+  @IsOptional()
+  @EnsureArray
+  @IsSchemaName({ each: true })
+  permissions?: string[];
+}

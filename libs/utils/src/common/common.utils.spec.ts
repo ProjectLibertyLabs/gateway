c6e832ada579e1ac714be39602801db70269add6
Aramik <aramikm@gmail.com>
2024-10-07 17:43:03 -0700

    bugfix: fixed content hash

--- libs/utils/src/common/common.utils.spec.ts
+++ libs/utils/src/common/common.utils.spec.ts
@@ -3,6 +3,6 @@ import { calculateDsnpMultiHash } from '#utils/common/common.utils';
 describe('common utils Tests', () => {
   it('hashes sha256 correctly ABC and returns a multihash value', async () => {
     const mb = await calculateDsnpMultiHash(Buffer.from('abc'));
-    expect(mb).toMatch('bcirbeif2pall7dybz7vecqka3zo24irdwabwdi4wc55jznaq75q7eaavvu');
+    expect(mb).toMatch('bciqlu6awx6hqdt7kifaubxs5vyrchmadmgrzmf32ts2bb73b6iablli');
   });
 });

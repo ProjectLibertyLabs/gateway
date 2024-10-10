875124715aad41bce80ba2658299accdb3435ca6
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-10-09 18:34:34 -0600

    wip: user_data script not running using from terraform registry

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -10,20 +10,38 @@ terraform {
 }
 
 # Conditional module loading based on cloud provider
-module "aws_infrastructure" {
-  source        = "./modules/aws"
-  orchestration = var.orchestration
-  count         = var.cloud_provider == "aws" ? 1 : 0
-  swarm_manager_node_count = var.swarm_manager_node_count
-  swarm_worker_node_count = var.swarm_worker_node_count
+# module "aws_infrastructure" {
+#   source                   = "./modules/aws"
+#   orchestration            = var.orchestration
+#   count                    = var.cloud_provider == "aws" ? 1 : 0
+#   swarm_manager_node_count = var.swarm_manager_node_count
+#   swarm_worker_node_count  = var.swarm_worker_node_count
+#   cluster_name         = var.cluster_name
+#   node_count           = var.node_count
+# }
+
+module "cluster" {
+  source    = "weibeld/kubeadm/aws"
+  vpc_id    = var.vpc_id
+  subnet_id = var.subnet_id
+  public_key_file = var.public_key_file
+  private_key_file = var.private_key_file
+  cluster_name = var.cluster_name
+  master_instance_type = "t2.micro"
+  worker_instance_type = "t2.micro"
 }
 
-module "local_infrastructure" {
-  source        = "./modules/local"
-  orchestration = var.orchestration
-  count         = var.cloud_provider == "local" ? 1 : 0
-}
+# module "local_infrastructure" {
+#   source        = "./modules/local"
+#   orchestration = var.orchestration
+#   count         = var.cloud_provider == "local" ? 1 : 0
+# }
 
 provider "aws" {
   region = "us-east-2"
 }
+
+# REMOVE: terraform has pruned this provider as not needed
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -1,23 +1,37 @@
-variable "swarm_manager_node_count" {
-  description = "Number of manager nodes to create"
-  type = number
-  default = 1
+#------------------------------------------------------------------------------#
+# Required variables
+#------------------------------------------------------------------------------#
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of an existing VPC in which to create the cluster."
 }
 
-variable "swarm_worker_node_count" {
-  description = "Number of worker nodes to create"
-  type = number
-  default = 2
+variable "subnet_id" {
+  type        = string
+  description = "ID of an existing subnet in which to create the cluster. The subnet must be in the VPC specified in the \"vpc_id\" variable, otherwise an error occurs."
 }
 
-variable "orchestration" {
-  description = "Type of orchestration to use (docker_swarm or kubernetes)"
-  type = string
-  default = "docker_swarm"
+variable "public_key_file" {
+  type        = string
+  description = "Path to the public key file on your local machine. This key pair will allow you to connect to the nodes of the cluster with SSH."
 }
 
-variable "cloud_provider" {
-  description = "The cloud provider to use for the infrastructure module"
-  type    = string
-  default = "aws"
+variable "private_key_file" {
+  type        = string
+  description = "Path to the private key file on your local machine. This key pair will allow you to connect to the nodes of the cluster with SSH."
+}
+variable "cluster_name" {
+  type        = string
+  description = "Name of the Kubernetes cluster to create. This name will be used in the names and tags of the created AWS resources and for the local kubeconfig file."
+}
+
+#------------------------------------------------------------------------------#
+# Optional variables
+#------------------------------------------------------------------------------#
+
+variable "region" {
+  type        = string
+  description = "AWS region in which to create the cluster."
+  default     = "us-east-2"
 }

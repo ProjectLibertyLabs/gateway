60e70e673401d6e20b5933b751932262a0a90719
Aramik <aramikm@gmail.com>
2024-10-18 16:50:51 -0700

    graph-service: added connection type to request

--- libs/types/src/dtos/graph/graph-query-params.dto.ts
+++ libs/types/src/dtos/graph/graph-query-params.dto.ts
@@ -4,6 +4,7 @@ import { PrivacyType } from './privacy-type.enum';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsMsaId } from '#utils/decorators/is-msa-id.decorator';
+import { ConnectionType } from '#types/dtos/graph/connection-type.enum';
 
 export class GraphsQueryParamsDto {
   /**
@@ -24,6 +25,15 @@ export class GraphsQueryParamsDto {
   })
   @IsEnum(PrivacyType)
   privacyType: PrivacyType;
+
+  @ApiProperty({
+    description: 'Graph connection type to query (follow or friendship)',
+    enum: ConnectionType,
+    enumName: 'ConnectionType',
+    example: 'follow',
+  })
+  @IsEnum(ConnectionType)
+  connectionType: ConnectionType;
 
   /**
    * Graph encryption keypairs for the users requested in `dsnpIds`. (Only for `privacyType` === "private"

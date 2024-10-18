4da98f7bc97061f0cfcb227e7ee8ada64ab3a619
Aramik <aramikm@gmail.com>
2024-10-18 11:38:59 -0700

    account-service: some fixes (#624)
    # Problem
    
    closes #623 
    
    # Changes
    - Added a very simple webhook to docker which logs all the receiving
    calls so that we are able to observe the payloads and have an end to end
    success from api -> worker -> webhooks
    - Fixed a bug with `POST
    v1/keys/KeysControllerV1_AddNewPublicKeyAgreements` account ID which was
    throwing errors in case the account was in SS58 format
    - Fixed a bug related to `ItemActionType` probably introduced during
    refactor
    - Fixed an issue with docker build script that was trying to build an
    image for `libs` which is not expected.
    - Renamed `rust-webhook-server `to `webhook-servers`
    
    ## Screenshots (optional):
    
    - an example of webhook-logger logs
    ```
    2024-10-17 16:19:22 Mock server started! on 3001
    2024-10-17 16:53:37 ==== POST /webhooks/account-service
    2024-10-17 16:53:37 > Headers
    2024-10-17 16:53:37 {
    2024-10-17 16:53:37   accept: 'application/json, text/plain, */*',
    2024-10-17 16:53:37   'content-type': 'application/json',
    2024-10-17 16:53:37   'user-agent': 'axios/1.7.7',
    2024-10-17 16:53:37   'content-length': '136',
    2024-10-17 16:53:37   'accept-encoding': 'gzip, compress, deflate, br',
    2024-10-17 16:53:37   host: 'mock-webhook-logger:3001',
    2024-10-17 16:53:37   connection: 'keep-alive'
    2024-10-17 16:53:37 }
    2024-10-17 16:53:37 > Body
    2024-10-17 16:53:37 {"transactionType":"ADD_PUBLIC_KEY_AGREEMENT","providerId":"1","referenceId":"0Odh_YcbgH-qbIR1gFijvdqKyRc","msaId":"260","schemaId":"7"}
    2024-10-17 17:11:43 ==== POST /webhooks/account-service
    2024-10-17 17:11:43 > Headers
    2024-10-17 17:11:43 {
    2024-10-17 17:11:43   accept: 'application/json, text/plain, */*',
    2024-10-17 17:11:43   'content-type': 'application/json',
    2024-10-17 17:11:43   'user-agent': 'axios/1.7.7',
    2024-10-17 17:11:43   'content-length': '170',
    2024-10-17 17:11:43   'accept-encoding': 'gzip, compress, deflate, br',
    2024-10-17 17:11:43   host: 'mock-webhook-logger:3001',
    2024-10-17 17:11:43   connection: 'keep-alive'
    2024-10-17 17:11:43 }
    2024-10-17 17:11:43 > Body
    2024-10-17 17:11:43 {"transactionType":"ADD_KEY","providerId":"1","referenceId":"cSRJACH0vHroIdob3Pm6DeHFyME","msaId":"260","newPublicKey":"5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy"}
    
    
    ```

---
+++ webhook-servers/loggerServer.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const server = http.createServer();
+
+server
+  .on('request', (request, response) => {
+    let body = [];
+    request
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        console.log(`==== ${request.method} ${request.url}`);
+        console.log('> Headers');
+        console.log(request.headers);
+
+        console.log('> Body');
+        try {
+            console.log(JSON.stringify(JSON.parse(body), null, 2));
+        } catch (e) {
+            console.error(body);
+        }
+        response.end();
+      });
+  })
+  .listen(process.env.WEBHOOK_PORT ?? 3001);
+
+console.log('Mock server started! on ' + (process.env.WEBHOOK_PORT ?? 3001));

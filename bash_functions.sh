2d16be3abba3ace6a3b2c8b58ac2c6f9b44e07fc
Shannon Wells <shannonwells@users.noreply.github.com>
2024-10-22 13:03:20 -0700

    update start/stop to use imported bash_functions script (#619)
    ## Goal
    The goal of this PR is to update the start and stop scripts to use
    bash_functions.sh imported from Social App Template. Some of the output
    has been altered to make the width work out when using special
    characters.
    
    Closes #456

---
+++ bash_functions.sh
@@ -0,0 +1,320 @@
+#!/bin/bash
+
+BOX_WIDTH=96
+
+###################################################################################
+#  Wrangle grep because MacOS doesn't come with a PCRE-enabled grep by default.
+#  If we don't find one, disable our "pretty" output function.
+###################################################################################
+PCRE_GREP=
+if 2>/dev/null 1>/dev/null grep -q -P "foo"; then
+    PCRE_GREP=grep
+else
+    # Grep is not PCRE compatible, check for other greps
+    if command -v ggrep >/dev/null; then # MacOS Homebrew might have ggrep
+        PCRE_GREP=ggrep
+    elif command -v prce2grep > /dev/null; then # MacOS Homebrew could also have pcre2grep
+        PCRE_GREP=pcre2grep
+    fi
+fi
+
+if [ -z "${PCRE_GREP}" ]; then
+    cat << EOI
+WARNING: No PCRE-capable 'grep' utility found; pretty terminal output disabled.
+
+If you're on a Mac, try installing GNU grep:
+    brew install grep
+
+EOI
+  read -p 'Press any key to continue... '
+
+  OUTPUT='echo -e'
+else
+  OUTPUT="box_text -w ${BOX_WIDTH}"
+fi
+
+###################################################################################
+# yesno
+#
+# Description: Prompt the user with a question requiring a yes/no response. The
+#              prompt will be augmented with "? [y/n]: " (with capitalization reflecting
+#              the default response if the user just hits <Enter>)
+#
+# ${1} - Prompt string
+# ${2} - Default if response is empty (only first character matters)
+###################################################################################
+function yesno() {
+    DEFAULT=Y
+    DEFAULT_STR="Y/n"
+    if [ $# -gt 1 ]; then
+        if [[ ${2} =~ ^[^Yy] ]]; then
+            DEFAULT_STR="y/N"
+            DEFAULT=N
+        fi
+    fi
+    echo
+    read -n 1 -p "${1}? [${DEFAULT_STR}]: "
+    echo
+    if [[ ${REPLY} =~ ^[Yy]$ ]] ; then
+        return 0
+    elif [ -z "${REPLY}" -a ${DEFAULT} = Y ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+# Internal function to calculate the display width of a string considering some common wide characters
+function display_width() {
+    local str="$1"
+    local width=0
+    local char
+
+    for (( i=0; i<${#str}; i++ )); do
+        char="${str:i:1}"
+        if [ -z ${PCRE_GREP} ]; then
+          # Regular character, assume it takes one column
+          width=$((width + 1))
+        else
+          if echo "$char" | ${PCRE_GREP} -P -q '[^\x{00}-\x{7F}]' ; then
+            width=$((width + 2))
+          else
+            # Regular character, assume it takes one column
+            width=$((width + 1))
+          fi
+        fi
+    done
+
+    echo $width
+}
+
+
+
+###################################################################################
+# box_text
+#
+# Description: Output text surrounded by a box; padded with blank lines inside the
+#              top and bottom edges. If TEXT is omitted, it will read from stdin.
+#
+# Usage: box_text [-w <min_width>] [TEXT]
+#
+#    min_width - Pad the output text box to a least min_width characters.
+#                Default: do not pad
+###################################################################################
+box_text() {
+    local input
+    local min_width=0
+
+    # Parse the optional -w argument
+    while getopts ":w:" opt; do
+        case $opt in
+            w)
+                min_width=$OPTARG
+                ;;
+            \?)
+                echo "Invalid option: -$OPTARG" >&2
+                return 1
+                ;;
+            :)
+                echo "Option -$OPTARG requires an argument." >&2
+                return 1
+                ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+
+    if [ -z "$1" ]; then
+        # Read input from stdin if no arguments are provided
+        input=$(cat)
+    else
+        # Use the provided argument as input
+        input="$1"
+    fi
+
+    local IFS=$'\n'
+    # local lines=($input)
+    local lines=()
+
+    # Read the string into the array, preserving empty lines
+    while IFS= read -r line || [[ -n $line ]]; do
+        lines+=("$line")
+    done <<< "${input}"
+
+    local max_length=0
+
+    # Find the maximum length of a line, accounting for Unicode width
+    for line in "${lines[@]}"; do
+        line_length=$(display_width "$line")
+        if [ $line_length -gt $max_length ]; then
+            max_length=$line_length
+        fi
+    done
+
+    # Ensure the box width is at least the specified minimum width
+    max_length=$(( max_length > min_width ? max_length : min_width ))
+
+    # Top border
+    echo "┌$(printf '─%.0s' $(seq 1 $((max_length + 2))))┐"
+
+    # Print each line with padding
+    for line in "${lines[@]}"; do
+        printf "│ %-${max_length}s │\n" "$line"
+    done
+
+    # Bottom border
+    echo "└$(printf '─%.0s' $(seq 1 $((max_length + 2))))┘"
+}
+
+###################################################################################
+# box_text_attention
+#
+# Description: Like box texty, but output is surrounded by a box;
+# padded with blank lines and attention-getting characters.
+# ##################################################################################
+box_text_attention() {
+    local input
+    local min_width=0
+    local attention=false
+
+    # Parse the optional -w argument
+    while getopts ":w:" opt; do
+        case $opt in
+            w)
+                min_width=$OPTARG
+                ;;
+            \?)
+                echo "Invalid option: -$OPTARG" >&2
+                return 1
+                ;;
+            :)
+                echo "Option -$OPTARG requires an argument." >&2
+                return 1
+                ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+
+    if [ -z "$1" ]; then
+        # Read input from stdin if no arguments are provided
+        input=$(cat)
+    else
+        # Use the provided argument as input
+        input="$1"
+    fi
+
+    local IFS=$'\n'
+    # local lines=($input)
+    local lines=()
+
+    # Read the string into the array, preserving empty lines
+    while IFS= read -r line || [[ -n $line ]]; do
+        lines+=("$line")
+    done <<< "${input}"
+
+    local max_length=0
+
+    # Find the maximum length of a line, accounting for Unicode width
+    for line in "${lines[@]}"; do
+        line_length=$(display_width "$line")
+        if [ $line_length -gt $max_length ]; then
+          max_length=$line_length
+        fi
+    done
+
+    # Ensure the box width is at least the specified minimum width
+    max_length=$(( max_length > min_width ? max_length : min_width ))
+
+    # Top border
+    echo "┌$(printf '─%.0s' $(seq 1 $((max_length + 10))))┐"
+    echo "│ 🔗💠📡$(printf ' %.0s' $(seq 1 $((max_length + 2)))) │"
+
+    # Print each line with padding
+    for line in "${lines[@]}"; do
+        real_length=$(( max_length ))
+        printf "│ 🔗💠📡  %-${real_length}s │\n" "$line"
+    done
+
+    # Bottom border
+    echo "│ 🔗💠📡$(printf ' %.0s' $(seq 1 $((max_length + 2)))) │"
+    echo "└$(printf '─%.0s' $(seq 1 $((max_length + 10))))┘"
+}
+
+###################################################################################
+# ask_and_save
+#
+# Description: Prompt the user with a string, and save the response to the environment file
+#
+# ${1} - env var name to save to the ENV_FILE
+# ${2} - prompt string
+# ${3} - [optional] default response if user hits <Enter>
+# ${4} - [optional] hide input if true (Default: false)
+###################################################################################
+ask_and_save() {
+    local var_name=${1}
+    local prompt=${2}
+    local default_value=${3}
+    local hide_input=${4:-false}
+    local value=
+    local input=
+
+    if [ -z "${default_value}" ]
+    then
+        if [ "${hide_input}" = true ]
+        then
+            read -rsp $'\n'"${prompt} (INPUT HIDDEN): " input
+            echo
+        else
+            read -rp $'\n'"${prompt}: " input
+        fi
+        value=${input}
+    else
+        if [ "${hide_input}" = true ]
+        then
+            read -rsp $'\n'"${prompt} [${default_value}] (INPUT HIDDEN): " input
+            echo
+        else
+            read -rp $'\n'"${prompt} [${default_value}]: " input
+        fi
+        value=${input:-$default_value}
+    fi
+    # Make the variable available in the current shell, useful for subsequent commands
+    export ${var_name}="${value}"
+    echo "${var_name}=\"${value}\"" >> ${ENV_FILE}
+}
+
+###################################################################################
+# redact_sensitive_values
+#
+# Description: Runs a 'sed' command on the input file to replace specific environment
+#              values with "[REDACTED]". Useful for not exposing secrets in live demos.
+#
+###################################################################################
+function redact_sensitive_values() {
+    local env_file="$1"
+    sed \
+        -e 's/^PROVIDER_ACCOUNT_SEED_PHRASE=.*/PROVIDER_ACCOUNT_SEED_PHRASE=[REDACTED]/' \
+        -e 's/^IPFS_BASIC_AUTH_USER=.*/IPFS_BASIC_AUTH_USER=[REDACTED]/' \
+        -e 's/^IPFS_BASIC_AUTH_SECRET=.*/IPFS_BASIC_AUTH_SECRET=[REDACTED]/' \
+        "$env_file"
+}
+
+###################################################################################
+#
+# Description: Save a name/value pair to the environment file & simultaneously
+#              export to the current environment.
+#
+# ${1} - env variable name
+# ${2} - env variable value
+#
+###################################################################################
+function export_save_variable() {
+    local variable_name=$1
+    local value=$2
+
+    echo "${variable_name}=\"${value}\"" >> ${ENV_FILE}
+    export ${variable_name}="${value}"
+}
+
+
--- start.sh
+++ start.sh
@@ -1,85 +1,67 @@
 #!/bin/bash
+. ./bash_functions.sh
 # Script to start all Gateway services on the Frequency Paseo Testnet
-
-# Function to ask for input with a default value and write to .env-saved
-ask_and_save() {
-    local var_name=${1}
-    local prompt=${2}
-    local default_value=${3}
-    read -rp $'\n'"${prompt} [${default_value}]: " input
-    local value=${input:-$default_value}
-    echo "${var_name}=\"${value}\"" >> .env-saved
-}
 
 # Check for Docker and Docker Compose
 if ! command -v docker &> /dev/null || ! command -v docker compose &> /dev/null; then
-    printf "Docker and Docker Compose are required but not installed. Please install them and try again.\n"
+    ${OUTPUT} "Docker and Docker Compose are required but not installed. Please install them and try again.\n"
     exit 1
 fi
 
+BASE_DIR=./
+ENV_FILE=${BASE_DIR}/.env-saved
+COMPOSE_PROJECT_NAME=${BASE_NAME}
+
+if [[ -n $ENV_FILE ]]; then
+    ${OUTPUT} "Using environment file: $ENV_FILE\n"
+fi
+
+
 # Load existing .env-saved file if it exists
 if [ -f .env-saved ]; then
-    echo -e "Found saved environment from a previous run:\n"
-    cat .env-saved
+    ${OUTPUT} "Found saved environment from a previous run:\n"
+    redact_sensitive_values .env-saved
     echo
-    read -p  "Do you want to re-use the saved paramters? [Y/n]: " REUSE_SAVED
+    read -p  "Do you want to re-use the saved parameters? [Y/n]: " REUSE_SAVED
     REUSE_SAVED=${REUSE_SAVED:-y}
 
-    if [[ ${REUSE_SAVED} =~ ^[Yy] ]]
-    then
-        cat << EOI
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ Loading existing .env-saved file environment values...                                      ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-EOI
+    if [[ ${REUSE_SAVED} =~ ^[Yy] ]]; then
+      ${OUTPUT} 'Loading existing .env-saved file environment values...'
     else
-        cat << EOI
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ Removing previous saved environment...                                                      |
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-EOI
+      ${OUTPUT} 'Removing previous saved environment...'
     rm .env-saved
     fi
 fi
 
-if [ ! -f .env-saved ]
-then
+if [ ! -f .env-saved ]; then
     # Setup some variables for easy port management
     STARTING_PORT=3010
     for i in {0..10}
     do
     eval SERVICE_PORT_${i}=$(( STARTING_PORT + i ))
-    eval "export SERVICE_PORT_${i}=\${SERVICE_PORT_${i}}"
-    eval "echo SERVICE_PORT_${i}=\${SERVICE_PORT_${i}}" >> .env-saved
+    export_save_variable SERVICE_PORT_${i} $(( STARTING_PORT + i ))
     done
 
     # Create .env-saved file to store environment variables
-    cat << EOI
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ Creating .env-saved file to store environment variables...                                  ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-EOI
+   ${OUTPUT} 'Creating .env-saved file to store environment variables... '
     echo "COMPOSE_PROJECT_NAME='gateway-dev'" >> .env-saved
+
     # Ask the user if they want to start on testnet or local
     read -p "Do you want to start on Frequency Paseo Testnet [y/N]: " TESTNET_ENV
-    echo "TESTNET_ENV=\"$TESTNET_ENV\"" >> .env-saved
+    export_save_variable TESTNET_ENV ${TESTNET_ENV}
 
-    if [[ $TESTNET_ENV =~ ^[Yy]$ ]]
-    then
-    cat << EOI
-
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ Setting defaults for testnet...                                                             ┃
-┃ Hit <ENTER> to accept the default value or enter new value and then hit <ENTER>             ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
+    if [[ $TESTNET_ENV =~ ^[Yy]$ ]]; then
+        ${OUTPUT} << EOI
+Setting defaults for testnet...
+Hit <ENTER> to accept the default value, or,
+enter new value and then hit <ENTER>"
 EOI
         DEFAULT_TESTNET_ENV="testnet"
         DEFAULT_FREQUENCY_API_WS_URL="wss://0.rpc.testnet.amplica.io"
         DEFAULT_SIWF_NODE_RPC_URL="https://0.rpc.testnet.amplica.io"
         DEFAULT_PROVIDER_ID="INPUT REQUIRED"
         DEFAULT_PROVIDER_ACCOUNT_SEED_PHRASE="INPUT REQUIRED"
+
     else
         echo -e "\nStarting on local..."
         DEFAULT_TESTNET_ENV="local"
@@ -88,6 +70,7 @@ EOI
         DEFAULT_PROVIDER_ID="1"
         DEFAULT_PROVIDER_ACCOUNT_SEED_PHRASE="//Alice"
     fi
+
     DEFAULT_IPFS_VOLUME="/data/ipfs"
     DEFAULT_IPFS_ENDPOINT="http://ipfs:5001"
     DEFAULT_IPFS_GATEWAY_URL='https://ipfs.io/ipfs/[CID]'
@@ -96,36 +79,21 @@ EOI
     DEFAULT_IPFS_UA_GATEWAY_URL="http://localhost:8080"
     DEFAULT_CONTENT_DB_VOLUME="content_db"
 
-
     ask_and_save FREQUENCY_API_WS_URL "Enter the Frequency API Websocket URL" "$DEFAULT_FREQUENCY_API_WS_URL"
     ask_and_save SIWF_NODE_RPC_URL "Enter the SIWF Node RPC URL" "$DEFAULT_SIWF_NODE_RPC_URL"
-cat << EOI
+    box_text_attention -w ${BOX_WIDTH} << EOI
+A Provider is required to start the services.
 
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃   🔗💠📡                                                                           📡💠🔗   ┃
-┃   🔗💠📡   A Provider is required to start the services.                           📡💠🔗   ┃
-┃   🔗💠📡                                                                           📡💠🔗   ┃
-┃   🔗💠📡   If you need to become a provider, visit                                 📡💠🔗   ┃
-┃   🔗💠📡   https://provider.frequency.xyz/ to get a Provider ID.                   📡💠🔗   ┃
-┃   🔗💠📡                                                                           📡💠🔗   ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
+If you need to become a provider, visit
+https://provider.frequency.xyz/ to get a Provider ID.
 EOI
+
     ask_and_save PROVIDER_ID "Enter Provider ID" "$DEFAULT_PROVIDER_ID"
     ask_and_save PROVIDER_ACCOUNT_SEED_PHRASE "Enter Provider Seed Phrase" "$DEFAULT_PROVIDER_ACCOUNT_SEED_PHRASE"
-    cat << EOI
 
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ Do you want to change the IPFS settings [y/N]:                                              ┃
-┃                                                                                             ┃
-┃ Suggestion: Change to an IPFS Pinning Service for better persistence and availability       ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+   ${OUTPUT} "Suggestion: Change to an IPFS Pinning Service for better persistence and availability."
 
-EOI
-    read CHANGE_IPFS_SETTINGS
-
-    if [[ $CHANGE_IPFS_SETTINGS =~ ^[Yy]$ ]]
-    then
+    if yesno "===> Given this suggestion, would you like to change the IPFS settings?";  then
         ask_and_save IPFS_VOLUME "Enter the IPFS volume" "$DEFAULT_IPFS_VOLUME"
         ask_and_save IPFS_ENDPOINT "Enter the IPFS Endpoint" "$DEFAULT_IPFS_ENDPOINT"
         ask_and_save IPFS_GATEWAY_URL "Enter the IPFS Gateway URL" "$DEFAULT_IPFS_GATEWAY_URL"
@@ -143,11 +111,12 @@ IPFS_BASIC_AUTH_SECRET="${DEFAULT_IPFS_BASIC_AUTH_SECRET}"
 IPFS_UA_GATEWAY_URL="${DEFAULT_IPFS_UA_GATEWAY_URL}"
 EOI
     fi
+
 fi
+
 set -a; source .env-saved; set +a
 
-if [[ ! $TESTNET_ENV =~ ^[Yy]$ ]]
-then
+if [[ ! $TESTNET_ENV =~ ^[Yy]$ ]]; then
     # Start specific services in detached mode
     echo -e "\nStarting local frequency services..."
     docker compose up -d frequency
@@ -157,28 +126,25 @@ fi
 echo -e "\nStarting all services..."
 docker compose  --profile webhook --profile backend up -d
 
-cat << EOI
+box_text_attention -w ${BOX_WIDTH} <<EOI
+You can access the Gateway at the following local addresses:
+* account-service:
+  - API:              http://localhost:${SERVICE_PORT_3}
+  - Queue management: http://localhost:${SERVICE_PORT_3}/queues
+  - Swagger UI:       http://localhost:${SERVICE_PORT_3}/docs/swagger
 
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ 🚀 You can access the Gateway at the following local addresses: 🚀                          ┃
-┃       * account-service:                                                                    ┃
-┃         - API:              http://localhost:${SERVICE_PORT_3}                                           ┃
-┃         - Queue management: http://localhost:${SERVICE_PORT_3}/queues                                    ┃
-┃         - Swagger UI:       http://localhost:${SERVICE_PORT_3}/docs/swagger                              ┃
-┃                                                                                             ┃
-┃       * content-publishing-service                                                          ┃
-┃         - API:              http://localhost:${SERVICE_PORT_0}                                           ┃
-┃         - Queue management: http://localhost:${SERVICE_PORT_0}/queues                                    ┃
-┃         - Swagger UI:       http://localhost:${SERVICE_PORT_0}/docs/swagger                              ┃
-┃                                                                                             ┃
-┃       * content-watcher-service                                                             ┃
-┃         - API:              http://localhost:${SERVICE_PORT_1}                                           ┃
-┃         - Queue management: http://localhost:${SERVICE_PORT_1}/queues                                    ┃
-┃         - Swagger UI:       http://localhost:${SERVICE_PORT_1}/docs/swagger                              ┃
-┃                                                                                             ┃
-┃       * graph-service                                                                       ┃
-┃         - API:              http://localhost:${SERVICE_PORT_2}                                           ┃
-┃         - Queue management: http://localhost:${SERVICE_PORT_2}/queues                                    ┃
-┃         - Swagger UI:       http://localhost:${SERVICE_PORT_2}/docs/swagger                              ┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+* content-publishing-service
+  - API:              http://localhost:${SERVICE_PORT_0}
+  - Queue management: http://localhost:${SERVICE_PORT_0}/queues
+  - Swagger UI:       http://localhost:${SERVICE_PORT_0}/docs/swagger
+
+* content-watcher-service
+  - API:              http://localhost:${SERVICE_PORT_1}
+  - Queue management: http://localhost:${SERVICE_PORT_1}/queues
+  - Swagger UI:       http://localhost:${SERVICE_PORT_1}/docs/swagger
+
+* graph-service
+  - API:              http://localhost:${SERVICE_PORT_2}
+  - Queue management: http://localhost:${SERVICE_PORT_2}/queues
+  - Swagger UI:       http://localhost:${SERVICE_PORT_2}/docs/swagger
 EOI

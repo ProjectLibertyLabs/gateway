95cf1c85b2e0e1f71e395574c4d9e57991b19f7d
Joe Caputo <joseph.caputo@unfinished.com>
2024-10-23 11:20:58 -0400

    fix: lint

--- apps/account-api/test/handles.controller.e2e-spec.ts
+++ apps/account-api/test/handles.controller.e2e-spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
-import { HttpStatus, INestApplication, Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { HttpStatus, ValidationPipe, VersioningType } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import request from 'supertest';

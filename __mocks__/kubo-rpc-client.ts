4e4d82d5b9fd8a19972b5015629955a20f2e0d2a
Joe Caputo <joseph.caputo@projectliberty.io>
2025-03-19 12:15:29 -0400

    fix: tests for IpfsService

--- __mocks__/kubo-rpc-client.ts
+++ __mocks__/kubo-rpc-client.ts
@@ -1,16 +1,5 @@
-export interface BlockStatResult {
-  cid: string;
-  size: number;
-  cumulativeSize: number;
-  blocks: number;
-  type: string;
-}
-
-export interface KuboRPCClient {
-  block: {
-    stat: (cid: string) => Promise<BlockStatResult>;
-  };
-}
+export const dummyCidV0 = 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR';
+export const dummyCidV1 = 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi';
 
 export class CID {
   // eslint-disable-next-line no-empty-function
@@ -21,8 +10,45 @@ export class CID {
   }
 
   static parse(cid: string): CID {
+    if (cid === 'bad cid') {
+      throw new SyntaxError('Non-base32 character');
+    }
     return new CID(cid);
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  toV0(): CID {
+    return new CID(dummyCidV0);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  toV1(): CID {
+    return new CID(dummyCidV1);
+  }
+}
+
+export interface BlockStatResult {
+  cid: string;
+  size: number;
+}
+
+export interface PinLsResult {
+  cid: CID;
+  type: string;
+  metadata?: Record<string, any>;
+  name?: string;
+}
+
+export interface KuboRPCClient {
+  block: {
+    stat: (cid: string) => Promise<BlockStatResult>;
+  };
+  cat: (cid: string) => AsyncIterable<Uint8Array>;
+  pin: {
+    ls: (options: any) => AsyncIterable<PinLsResult>;
+  };
+  add: (entry: any, options?: any) => Promise<any>;
+  version: (options?: any) => Promise<string>;
 }
 
 export const createKuboRPCClient = jest.fn(
@@ -36,5 +62,11 @@ export const createKuboRPCClient = jest.fn(
         type: 'mocked-type',
       })),
     },
+    cat: jest.fn(),
+    pin: {
+      ls: jest.fn(),
+    },
+    add: jest.fn(),
+    version: jest.fn(),
   }),
 );

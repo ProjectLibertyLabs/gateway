691cd6331cba8d35fb2af7447cf2a1a9ef5d8dcc
emresurmeli <s.emre.s.8@gmail.com>
2025-06-13 17:43:48 -0400

    Modify announceExistingBatch pinning to not fail when files haven't been pinned to local node

--- libs/storage/src/ipfs/ipfs.service.ts
+++ libs/storage/src/ipfs/ipfs.service.ts
@@ -74,14 +74,24 @@ export class IpfsService {
   }
 
   public async existsInLocalGateway(cid: string): Promise<boolean> {
-    this.logger.debug(`Requesting HEAD for ${cid}`);
-    const response = await fetch(getIpfsCidPlaceholder(cid, this.gatewayUrl), {
-      method: "HEAD",
-      headers: {
-        "Cache-Control": "only-if-cached",
-      },
-    });
-    return response && response.status === 200;
+    this.logger.debug(`Checking existence for ${cid}`);
+    try {
+      // First try a direct HEAD request without cache restriction
+      const response = await fetch(getIpfsCidPlaceholder(cid, this.gatewayUrl), {
+        method: "HEAD"
+      });
+      
+      if (response.ok) {
+        return true;
+      }
+
+      // If not found in gateway, check if we can get it from the IPFS network
+      const stat = await this.ipfs.block.stat(CID.parse(cid));
+      return !!stat.cid;
+    } catch (err) {
+      this.logger.debug(`Failed to verify existence of ${cid}: ${err}`);
+      return false;
+    }
   }
 
   public async isPinned(cid: string): Promise<boolean> {
@@ -203,5 +213,17 @@ export class IpfsService {
         return fn();
       })
       .finally(() => clearTimeout(timeout));
+  }
+
+  public async tryPin(cid: string): Promise<boolean> {
+    try {
+      this.logger.debug(`Attempting to pin ${cid}`);
+      const parsedCid = CID.parse(cid);
+      await this.ipfs.pin.add(parsedCid);
+      return true;
+    } catch (err) {
+      this.logger.debug(`Failed to pin ${cid}: ${err}`);
+      return false;
+    }
   }
 }

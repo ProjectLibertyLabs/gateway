641ffb438700b7eed36982b715a218cb0434430b
Hady Wahby <15131750+hwahby@users.noreply.github.com>
2025-06-24 13:47:08 -0700

    feat: health check library (#854)
    # Problem
    
    heathz or some other debugging helpful endpoint that answers common
    questions:
    
    - mainnet or testnet or other
    - non-sensitive env vars/config values
    - queue statuses
    - utc timestamp
    - current block head or other chain info
    - etc...
    
    problem statement, including
    Link to GitHub Issue(s):
    [#734](https://github.com/ProjectLibertyLabs/gateway/issues/734)
    
    # Solution
    
    Create new library that using config, queue, and blockchain libraries to
    queue service health information. Open to suggestions on addition data
    that would be helpful for troubleshoot customer systems.
    
    ## Steps to Verify:
    
    1. Run ./start.sh, accept defaults.
    
    1. Make request to `localhost:3010/healthz` to verify that the
    content-publishing-api `/healthz` endpoint works
    
    ## Screenshots (optional):
    `localhost:3010/healthz` against `content-publishing-api`
    
    ![Screenshot 2025-06-16 at 4 11
    45 PM](https://github.com/user-attachments/assets/30473ced-a8ae-451b-a839-426b502049f2)
    
    ---------
    
    Co-authored-by: hwahby <hwahby@users.noreply.github.com>

--- apps/content-publishing-api/src/controllers/health.controller.ts
+++ apps/content-publishing-api/src/controllers/health.controller.ts
@@ -1,19 +1,36 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HealthCheckService } from '#health-check/health-check.service';
+import { HealthResponseDto } from '#types/dtos/common/health.response.dto';
+import { ContentPublishingQueues as QueueConstants } from '#types/constants/queue.constants';
 
 @Controller()
 @ApiTags('health')
 export class HealthController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    // eslint-disable-next-line no-empty-function
+  ) {}
+
   // Health endpoint
   // eslint-disable-next-line class-methods-use-this
   @Get('healthz')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Check the health status of the service' })
   @ApiOkResponse({ description: 'Service is healthy' })
-  healthz() {
+  async healthz(): Promise<HealthResponseDto> {
     return {
       status: HttpStatus.OK,
       message: 'Service is healthy',
+      timestamp: Date.now(),
+      config: this.healthCheckService.getServiceConfig('content-publishing-api'),
+      queueStatus: await this.healthCheckService.getQueueStatus([
+        QueueConstants.REQUEST_QUEUE_NAME,
+        QueueConstants.ASSET_QUEUE_NAME,
+        QueueConstants.PUBLISH_QUEUE_NAME,
+        QueueConstants.BATCH_QUEUE_NAME,
+      ]),
+      blockchainStatus: await this.healthCheckService.getBlockchainStatus(),
     };
   }
 

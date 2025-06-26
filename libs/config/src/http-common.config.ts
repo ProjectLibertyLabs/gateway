c09a8168fe71a59566f7e09b6e144bd0f5d51f21
Emre Surmeli <s.emre.s.8@gmail.com>
2025-06-25 22:24:13 -0400

    Modify the http config to make sure that http timeout is always less … (#863)
    # Problem
    Issue #779 
    
    We want to make sure that http calls(axios requests) going out of the
    gateway timeout before the API_TIMEOUT_MS
    
    # Solution
    
    Currently, the API_TIMEOUT_MS default is 30 seconds throughout the
    gateway, and the HTTP_REQUEST_TIMEOUT_MS default is 3 seconds. In this
    PR I enforce some config validation so that in case the http timeout is
    greater than api timeout, we get some errors. I also provide some tests
    to make sure that the validation works.
    
    # Additional Considerations
    
    We might want to implement an HttpModule that uses @nestjs/axios package
    to create a central axios implementation that uses interceptors to set
    http timeouts that's central. But maybe we don't want that since
    different services wish to set different http timeouts? Check
    https://docs.nestjs.com/techniques/http-module for context.

--- libs/config/src/http-common.config.ts
+++ libs/config/src/http-common.config.ts
@@ -7,10 +7,24 @@ export interface IHttpCommonConfig {
 }
 
 export default registerAs('http-common', (): IHttpCommonConfig => {
+  // Get API_TIMEOUT_MS from environment or use default
+  const apiTimeoutMs = parseInt(process.env.API_TIMEOUT_MS || '30000', 10);
+
   const configs: JoiUtils.JoiConfig<IHttpCommonConfig> = JoiUtils.normalizeConfigNames({
     httpResponseTimeoutMS: {
       label: 'HTTP_RESPONSE_TIMEOUT_MS',
-      joi: Joi.number().min(0).default(3000),
+      joi: Joi.number()
+        .min(0)
+        .max(apiTimeoutMs - 1) // Must be less than API_TIMEOUT_MS
+        .default(3000)
+        .custom((value, helpers) => {
+          if (value >= apiTimeoutMs) {
+            return helpers.error('any.custom', {
+              message: `HTTP_RESPONSE_TIMEOUT_MS (${value}) must be less than API_TIMEOUT_MS (${apiTimeoutMs})`,
+            });
+          }
+          return value;
+        }),
     },
   });
 

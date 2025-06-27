6c5e8fb94a3bb188c0ea63eb7cac313a77ee3b9e
shannonwells <shannonwells@users.noreply.github.com>
2025-06-26 17:57:40 -0700

    cleanup

--- libs/blockchain/src/polkadot-api.service.ts
+++ libs/blockchain/src/polkadot-api.service.ts
@@ -125,12 +125,7 @@ export class PolkadotApiService extends EventEmitter2 implements OnApplicationSh
   }
 
   public async isReady(): Promise<boolean> {
-    const birp = await this.baseIsReadyPromise;
-    this.logger.debug('baseIsReadyPromise');
-    const api = await this.api.isReady;
-    this.logger.debug('api is ready');
-    return birp && !!api;
-    // return (await this.baseIsReadyPromise) && !!(await this.api.isReady);
+    return (await this.baseIsReadyPromise) && !!(await this.api.isReady);
   }
 
   private startDisconnectedTimeout(isInit = false) {

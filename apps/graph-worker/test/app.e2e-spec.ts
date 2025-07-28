2bf6df6b3857d2d1b20bf9fab29db575ca4da87f
hwahby <hwahby@users.noreply.github.com>
2025-07-28 09:43:43 -0700

    feat: add worker e2e test suites

---
+++ apps/graph-worker/test/app.e2e-spec.ts
@@ -0,0 +1,106 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { WorkerModule } from '../src/worker.module';
+import WorkerConfig, { IGraphWorkerConfig } from '#graph-worker/worker.config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { TimeoutInterceptor } from '#utils/interceptors/timeout.interceptor';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+describe('Graph Worker E2E request verification!', () => {
+  let app: NestExpressApplication;
+  let module: TestingModule;
+  let httpServer: any;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [WorkerModule],
+    }).compile();
+
+    app = module.createNestApplication();
+
+    // Uncomment below to see logs when debugging tests
+    // module.useLogger(new Logger());
+
+    const config = app.get<IGraphWorkerConfig>(WorkerConfig.KEY);
+    app.enableVersioning({ type: VersioningType.URI });
+    app.enableShutdownHooks();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, enableDebugMessages: true }));
+    app.useGlobalInterceptors(new TimeoutInterceptor(config.apiTimeoutMs));
+    app.useBodyParser('json', { limit: config.apiBodyJsonLimit });
+
+    const eventEmitter = app.get<EventEmitter2>(EventEmitter2);
+    eventEmitter.on('shutdown', async () => {
+      await app.close();
+    });
+    await app.init();
+
+    httpServer = app.getHttpServer();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await httpServer.close();
+
+    // Wait for some pending async stuff to finish
+    await new Promise<void>((resolve) => {
+      setTimeout(() => resolve(), 1000);
+    });
+  });
+
+  it('(GET) /healthz', () =>
+    request(httpServer)
+      .get('/healthz')
+      .expect(200)
+      .then((res) => {
+        const baseConfigExpectation: { [key: string]: any } = {
+          apiBodyJsonLimit: expect.any(String),
+          workerApiPort: expect.any(Number),
+          apiTimeoutMs: expect.any(Number),
+          webhookFailureThreshold: expect.any(Number),
+          webhookRetryIntervalSeconds: expect.any(Number),
+        };
+
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            status: 200,
+            message: 'Service is healthy',
+            timestamp: expect.any(Number),
+            config: expect.objectContaining(baseConfigExpectation),
+            redisStatus: expect.objectContaining({
+              connected_clients: expect.any(Number),
+              maxmemory: expect.any(Number),
+              redis_version: expect.any(String),
+              uptime_in_seconds: expect.any(Number),
+              used_memory: expect.any(Number),
+              queues: expect.arrayContaining([
+                expect.objectContaining({
+                  name: expect.any(String),
+                  waiting: expect.any(Number),
+                  active: expect.any(Number),
+                  completed: expect.any(Number),
+                  failed: expect.any(Number),
+                  delayed: expect.any(Number),
+                }),
+              ]),
+            }),
+            blockchainStatus: expect.objectContaining({
+              frequencyApiWsUrl: expect.any(String),
+              latestBlockHeader: expect.objectContaining({
+                blockHash: expect.any(String),
+                number: expect.any(Number),
+                parentHash: expect.any(String),
+              }),
+            }),
+          }),
+        );
+      }));
+
+  it('(GET) /livez', () =>
+    request(httpServer).get('/livez').expect(200).expect({ status: 200, message: 'Service is live' }));
+
+  it('(GET) /readyz', () =>
+    request(httpServer).get('/readyz').expect(200).expect({ status: 200, message: 'Service is ready' }));
+
+  it('(GET) /metrics', () => request(httpServer).get('/metrics').expect(200));
+});

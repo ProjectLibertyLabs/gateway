8426d5dd2a548b5496c6cddb860c8d718922a637
Joe Caputo <joseph.caputo@projectliberty.io>
2025-08-07 15:42:22 -0400

    fix: do not return a Promise inside try...catch (#916)
    # Problem
    A missing `await` was causing errors to be mis-handled.
    
    # Details
    Normally, in an `async` Javascript method, it is not necessary to
    'await' a value that is being returned; the logic being that since an
    async function wraps its return value in a Promise anyway, returning a
    Promise directly is transparent to the caller. So, the following is
    perfectly fine:
    ```javascript
    async function foo(): Promise<number> {
      // do some async stuff
      return value;
    }
    
    async function bar(): Promise<number> {
      return foo();
    }
    ```
    
    In the above, 'bar' doesn't need to 'await foo()', because it will
    return `Promise<number>` regardless.
    
    However, if we modify the code as follows:
    ```javascript
    async function foo(): Promise<number> {
      // do some async stuff
      if (has_error) {
        throw new Error('I screwed up');
      }
    
      return value;
    }
    
    async bar(): Promise<number> {
       try {
        return foo();
      } catch (err: any) {
        throw Error('foo screwed up');
      }
    }
    ```
    
    In the above, the statement `return foo();` will **always** return a
    `Promise<number>`. If 'foo' throws, any callers of 'bar', if they
    'await' or '.catch(...)' the return value, will only ever get the error
    'I screwed up'. The error 'foo screwed up' will **never** be thrown,
    because 'foo' is always a Promise that hasn't resolved or rejected yet
    at the time 'bar' returns.
    
    So, the moral of the story is: it's safe to return an encapsulated async
    function's value directly inside another async function _unless the call
    is inside a try...catch block_!

--- apps/content-publishing-worker/src/publisher/message.publisher.ts
+++ apps/content-publishing-worker/src/publisher/message.publisher.ts
@@ -3,8 +3,6 @@ import { ISubmittableResult } from '@polkadot/types/types';
 import { SubmittableExtrinsic } from '@polkadot/api-base/types';
 import { BlockchainService } from '#blockchain/blockchain.service';
 import { IPublisherJob, isIpfsJob } from '#types/interfaces/content-publishing';
-import { NonceConflictError } from '#blockchain/types';
-import { DelayedError } from 'bullmq';
 import { Logger, pino } from 'pino';
 import { getBasicPinoOptions } from '#logger-lib';
 
@@ -31,14 +29,6 @@ export class MessagePublisher {
     tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
   ): ReturnType<BlockchainService['payWithCapacity']> {
     this.logger.debug(`Submitting tx of size ${tx.length}`);
-    try {
-      return this.blockchainService.payWithCapacity(tx);
-    } catch (e) {
-      this.logger.error(`Error processing batch: ${e}`);
-      if (e instanceof NonceConflictError) {
-        throw new DelayedError();
-      }
-      throw e;
-    }
+    return this.blockchainService.payWithCapacity(tx);
   }
 }

d87a9066e95e535292ef0c4838ae2a59a949a45f
hwahby <hwahby@users.noreply.github.com>
2025-08-08 19:04:54 -0700

    feat: add dependency check to readyz request

--- apps/account-api/src/controllers/health.controller.ts
+++ apps/account-api/src/controllers/health.controller.ts
@@ -32,6 +32,7 @@ export class HealthController {
     return {
       status: HttpStatus.OK,
       message: 'Service is live',
+      timestamp: new Date().toISOString(),
     };
   }
 
@@ -42,9 +43,6 @@ export class HealthController {
   @ApiOperation({ summary: 'Check the ready status of the service' })
   @ApiOkResponse({ description: 'Service is ready' })
   readyz() {
-    return {
-      status: HttpStatus.OK,
-      message: 'Service is ready',
-    };
+    return this.healthCheckService.getServiceReadiness();
   }
 }

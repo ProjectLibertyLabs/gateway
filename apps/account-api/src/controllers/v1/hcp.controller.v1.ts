c5050c7b3d7d59231cd6e78fa9c7b462d027ca1a
shannonwells <shannonwells@users.noreply.github.com>
2025-11-26 14:16:39 -0800

    WIP fleshing out combined batch txn in the controller, will be moved

--- apps/account-api/src/controllers/v1/hcp.controller.v1.ts
+++ apps/account-api/src/controllers/v1/hcp.controller.v1.ts
@@ -1,43 +1,110 @@
 import { HcpService } from '#account-api/services/hcp.service';
-import { BlockchainRpcQueryService } from '#blockchain/blockchain-rpc-query.service';
-import { ApiService } from '#content-publishing-api/api.service';
-import { HcpPublishAllRequestDto } from '#types/dtos/account';
+import { ITxStatus } from '#account-lib/interfaces/tx-status.interface';
+import { IBlockchainConfig } from '#blockchain/blockchain.config';
+import { BlockchainService } from '#blockchain/blockchain.service';
+import { NonceConflictError } from '#blockchain/types';
+import { AddNewPublicKeyAgreementRequestDto, HcpPublishAllRequestDto, UpsertPagePayloadDto } from '#types/dtos/account';
 import { AccountIdDto } from '#types/dtos/common';
-import { AnnouncementResponseDto } from '#types/dtos/content-publishing';
 import {
   Body,
   Controller,
   HttpCode,
   HttpStatus,
+  Inject,
   NotFoundException,
   Param,
   Post,
   UnauthorizedException,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ApiPromise } from '@polkadot/api';
+import { SubmittableExtrinsic } from '@polkadot/api/types';
+import { Vec } from '@polkadot/types';
+import { AccountId, Call } from '@polkadot/types/interfaces';
+import { IMethod, ISubmittableResult } from '@polkadot/types/types';
+import { HexString } from '@polkadot/util/types';
+import { DelayedError } from 'bullmq';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Controller({ version: '1', path: 'hcp' })
 @ApiTags('v1/hcp')
 export class HcpControllerV1 {
   constructor(
-    private readonly apiService: ApiService,
-    private readonly hcpService: HcpService,
+    private readonly blockchainService: BlockchainService,
     @InjectPinoLogger(HcpControllerV1.name) private readonly logger: PinoLogger,
   ) {}
 
-  ///
-  ///
+  /// WIP This will get moved to a service and a separate queue like graph publisher,
+  // to handle capacity running out events
   @Post(':accountId/publishAll')
   @HttpCode(HttpStatus.ACCEPTED)
   async publishAll(
     @Param() { accountId }: AccountIdDto,
-    @Body() payload: HcpPublishAllRequestDto,
-  ): Promise<AnnouncementResponseDto> {
+    @Body() payloads: HcpPublishAllRequestDto,
+  ): Promise<HexString> {
     // check that the accountId has an MSA on chain as a fast, early failure.
     // it's not necessary to deserialize the payload to verify the id matches.
-    this.hcpService.verifyAccountHasMsa(accountId.toString());
+    this.verifyAccountHasMsa(accountId);
     // FA will check access control as soon as symmetric key is requested,
     // so ensure that there is no delay in a payWithCapacityBatchAll call.
+    return this.publishHcpMessages(accountId, payloads);
+  }
+
+  async verifyAccountHasMsa(accountId: string): Promise<void> {
+    const api = (await this.blockchainService.getApi()) as ApiPromise;
+    const res = await api.query.msa.publicKeyToMsaId(accountId);
+    if (res.isNone) {
+      throw new NotFoundException(`MSA ID for account ${accountId} not found`);
+    }
+  }
+
+  async publishHcpMessages(accountId: string, payloads: HcpPublishAllRequestDto): Promise<HexString> {
+    const api = (await this.blockchainService.getApi()) as ApiPromise;
+    let txns: Array<SubmittableExtrinsic<'promise', ISubmittableResult>> = [];
+    const callVec = this.blockchainService.createType('Vec<Call>', txns);
+    txns.push(this.buildApplyItemActionExtrinsic(accountId, payloads.addHcpPublicKeyPayload, api));
+    txns.push(this.buildApplyItemActionExtrinsic(accountId, payloads.addContextGroupPRIDEntryPayload, api));
+    txns.push(this.buildUpsertPageExtrinsic(accountId, payloads.addContentGroupMetadataPayload, api));
+    let [_tx, txHash, _blockNumber] = await this.processBatchTxn(callVec);
+    this.logger.debug(`txns: ${txns}`);
+    return txHash;
+  }
+
+  buildApplyItemActionExtrinsic(
+    accountId: string,
+    payload: AddNewPublicKeyAgreementRequestDto,
+    api: ApiPromise,
+  ): SubmittableExtrinsic<'promise', ISubmittableResult> {
+    return api.tx.statefulStorage.applyItemActionsWithSignatureV2(
+      accountId,
+      { Sr25519: payload.proof },
+      payload.payload,
+    );
+  }
+
+  buildUpsertPageExtrinsic(
+    accountId: string,
+    payload: UpsertPagePayloadDto,
+    api: ApiPromise,
+  ): SubmittableExtrinsic<'promise', ISubmittableResult> {
+    return api.tx.statefulStorage.upsertPageWithSignatureV2(accountId, { Sr25519: payload.proof }, payload.payload);
+  }
+
+  processBatchTxn(
+    callVec: Vec<Call> | (Call | IMethod | string | Uint8Array)[],
+  ): ReturnType<BlockchainService['payWithCapacityBatchAll']> {
+    this.logger.trace(
+      'processBatchTxn: callVec: ',
+      callVec.map((c) => c.toHuman()),
+    );
+    try {
+      return this.blockchainService.payWithCapacityBatchAll(callVec);
+    } catch (error: any) {
+      this.logger.error(`Error processing batch transaction: ${error}`);
+      if (error instanceof NonceConflictError) {
+        throw new DelayedError();
+      }
+      throw error;
+    }
   }
 }
--- apps/account-api/src/services/hcp.service.ts
+++ apps/account-api/src/services/hcp.service.ts
@@ -1,4 +1,3 @@
-import apiConfig, { IAccountApiConfig } from '#account-api/api.config';
 import blockchainConfig, { IBlockchainConfig } from '#blockchain/blockchain.config';
 import { BlockchainRpcQueryService } from '#blockchain/blockchain-rpc-query.service';
 import { AccountIdDto } from '#types/dtos/common';
@@ -10,17 +9,17 @@ export class HcpService {
   constructor(
     // needed to get providerId, keys, for signing etc
     @Inject(blockchainConfig.KEY) private readonly blockchainConf: IBlockchainConfig,
-    private blockchainService: BlockchainRpcQueryService,
+    private readonly blockchainService: BlockchainRpcQueryService,
     private readonly logger: PinoLogger,
   ) {
     this.logger.setContext(this.constructor.name);
   }
 
-  async verifyAccountHasMsa(accountId: AccountIdDto): Promise<void> {
-    const api = (await this.blockchainService.getApi()) as ApiPromise;
-    const res = await api.query.msa.publicKeyToMsaId(accountId);
-    if (res.isNone) {
-      throw new NotFoundException(`MSA ID for account ${accountId} not found`);
-    }
-  }
+  // async verifyAccountHasMsa(accountId: AccountIdDto): Promise<void> {
+  //   const api = (await this.blockchainService.getApi()) as ApiPromise;
+  //   const res = await api.query.msa.publicKeyToMsaId(accountId);
+  //   if (res.isNone) {
+  //     throw new NotFoundException(`MSA ID for account ${accountId} not found`);
+  //   }
+  // }
 }

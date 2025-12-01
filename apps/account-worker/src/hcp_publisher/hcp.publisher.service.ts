75d1e72b0a705c8d37307a49e6d4fbdba66ceeb1
shannonwells <shannonwells@users.noreply.github.com>
2025-12-01 12:41:34 -0800

    * Small refactor to a base publisher service * spellchecking update: add to dictionary, make it quiet

--- apps/account-worker/src/hcp_publisher/hcp.publisher.service.ts
+++ apps/account-worker/src/hcp_publisher/hcp.publisher.service.ts
@@ -1,30 +1,21 @@
 import { InjectRedis } from '@songkeys/nestjs-redis';
 import { InjectQueue, Processor } from '@nestjs/bullmq';
-import { Inject, Injectable, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { DelayedError, Job, Queue } from 'bullmq';
 import Redis from 'ioredis';
 import { SubmittableExtrinsic } from '@polkadot/api-base/types';
-import { IMethod, ISubmittableResult } from '@polkadot/types/types';
+import { ISubmittableResult } from '@polkadot/types/types';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { BlockchainService } from '#blockchain/blockchain.service';
-import { ICapacityInfo, NonceConflictError } from '#blockchain/types';
 import { AccountQueues as QueueConstants } from '#types/constants/queue.constants';
-import { BaseConsumer } from '#consumer';
 import { HcpPublishJob } from '#types/dtos/account';
 import { IHcpTxStatus } from '#types/interfaces/tx-status.interface';
 import { HexString } from '@polkadot/util/types';
-import {
-  CAPACITY_AVAILABLE_EVENT,
-  CAPACITY_EXHAUSTED_EVENT,
-  CapacityCheckerService,
-} from '#blockchain/capacity-checker.service';
-import { OnEvent } from '@nestjs/event-emitter';
-import { Vec } from '@polkadot/types';
-import { Call } from '@polkadot/types/interfaces';
-import { TXN_WATCH_LIST_KEY } from '#types/constants';
+import { CapacityCheckerService } from '#blockchain/capacity-checker.service';
 import workerConfig, { IAccountWorkerConfig } from '#account-worker/worker.config';
 import { PinoLogger } from 'nestjs-pino';
 import blockchainConfig, { IBlockchainConfig } from '#blockchain/blockchain.config';
+import { BasePublisherService } from '#queue';
 
 const CAPACITY_EPOCH_TIMEOUT_NAME = 'hcp_capacity_check';
 
@@ -33,33 +24,26 @@ const CAPACITY_EPOCH_TIMEOUT_NAME = 'hcp_capacity_check';
  */
 @Injectable()
 @Processor(QueueConstants.HCP_PUBLISH_QUEUE)
-export class HcpPublisherService extends BaseConsumer implements OnApplicationBootstrap, OnApplicationShutdown {
-  public async onApplicationBootstrap() {
-    await this.blockchainService.isReady();
-    await this.capacityCheckerService.checkForSufficientCapacity();
-    this.worker.concurrency = this.accountWorkerConfig[`${this.worker.name}QueueWorkerConcurrency`] || 1;
+export class HcpPublisherService extends BasePublisherService {
+  protected readonly capacityTimeoutName = CAPACITY_EPOCH_TIMEOUT_NAME;
+  protected readonly workerConcurrencyKey = `${QueueConstants.HCP_PUBLISH_QUEUE}QueueWorkerConcurrency`;
+
+  constructor(
+    @InjectRedis() cacheManager: Redis,
+    @InjectQueue(QueueConstants.HCP_PUBLISH_QUEUE) protected readonly queue: Queue,
+    @Inject(blockchainConfig.KEY) private readonly blockchainConf: IBlockchainConfig,
+    @Inject(workerConfig.KEY) protected readonly workerConfig: IAccountWorkerConfig,
+    blockchainService: BlockchainService,
+    capacityCheckerService: CapacityCheckerService,
+    schedulerRegistry: SchedulerRegistry,
+    logger: PinoLogger,
+  ) {
+    super(cacheManager, blockchainService, schedulerRegistry, capacityCheckerService, logger);
   }
 
   public async onApplicationShutdown(_signal?: string | undefined): Promise<void> {
-    await this.hcpPublishQueue.close();
-    try {
-      this.schedulerRegistry.deleteTimeout(CAPACITY_EPOCH_TIMEOUT_NAME);
-    } catch (_err) {
-      // ignore
-    }
-  }
-
-  constructor(
-    @InjectRedis() private cacheManager: Redis,
-    @InjectQueue(QueueConstants.HCP_PUBLISH_QUEUE) private hcpPublishQueue: Queue,
-    @Inject(blockchainConfig.KEY) private readonly blockchainConf: IBlockchainConfig,
-    @Inject(workerConfig.KEY) private readonly accountWorkerConfig: IAccountWorkerConfig,
-    private blockchainService: BlockchainService,
-    private capacityCheckerService: CapacityCheckerService,
-    private schedulerRegistry: SchedulerRegistry,
-    protected readonly logger: PinoLogger,
-  ) {
-    super(logger);
+    await this.queue.close();
+    await super.onApplicationShutdown(_signal);
   }
 
   /**
@@ -75,6 +59,10 @@ export class HcpPublisherService extends BaseConsumer implements OnApplicationBo
 
     try {
       this.logger.info(`Processing HCP job ${job.id}`);
+      // Check capacity first; if out of capacity, send job back to queue
+      if (!(await this.capacityCheckerService.checkForSufficientCapacity())) {
+        throw new DelayedError('Out of Capacity. Delaying HCP job ' + job.id);
+      }
       let blockNumber: number;
 
       // Decode the encoded extrinsics
@@ -100,95 +88,13 @@ export class HcpPublisherService extends BaseConsumer implements OnApplicationBo
         death: payWithCapacityTx.era.asMortalEra.death(blockNumber),
       };
 
-      const obj = {};
-      obj[payWithCapacityTxHash] = JSON.stringify(status);
-
-      await this.cacheManager.hset(TXN_WATCH_LIST_KEY, obj);
-
-      this.logger.debug('Cached extrinsic to monitor: ', payWithCapacityTxHash);
+      await this.cacheTransactionStatus(payWithCapacityTxHash, status);
     } catch (error: unknown) {
       if (error instanceof DelayedError) {
         job.moveToDelayed(Date.now(), job.token);
       }
       this.logger.error(error);
       throw error;
-    } finally {
-      await this.capacityCheckerService.checkForSufficientCapacity();
     }
-  }
-
-  /**
-   * Processes a batch transaction by submitting it to the blockchain.
-   *
-   * @param callVec The vector of calls to be batched.
-   * @returns The hash of the submitted transaction.
-   * @throws Error if the transaction hash is undefined or if there is an error processing the batch.
-   */
-  async processBatchTxn(
-    callVec: Vec<Call> | (Call | IMethod | string | Uint8Array)[],
-  ): ReturnType<BlockchainService['payWithCapacityBatchAll']> {
-    this.logger.trace(
-      'processBatchTxn: callVec: ',
-      callVec.map((c) => c.toHuman()),
-    );
-    try {
-      return this.blockchainService.payWithCapacityBatchAll(callVec);
-    } catch (error: any) {
-      this.logger.error(`Error processing batch transaction: ${error}`);
-      if (error instanceof NonceConflictError) {
-        throw new DelayedError();
-      }
-      throw error;
-    }
-  }
-
-  @OnEvent(CAPACITY_EXHAUSTED_EVENT)
-  public async handleCapacityExhausted(capacityInfo: ICapacityInfo) {
-    // Avoid spamming the log
-    if (!(await this.hcpPublishQueue.isPaused())) {
-      this.logger.warn('Capacity Exhausted: Pausing HCP publish queue until next epoch');
-    }
-    await this.hcpPublishQueue.pause();
-    const blocksRemaining = capacityInfo.nextEpochStart - capacityInfo.currentBlockNumber;
-    const epochTimeout = blocksRemaining * (this.blockchainService.blockTimeMs || 6000);
-    try {
-      // Check if a timeout with the same name already exists
-      this.schedulerRegistry.deleteTimeout(CAPACITY_EPOCH_TIMEOUT_NAME);
-
-      // Add the new timeout
-      this.schedulerRegistry.addTimeout(
-        CAPACITY_EPOCH_TIMEOUT_NAME,
-        setTimeout(() => this.capacityCheckerService.checkForSufficientCapacity(), epochTimeout),
-      );
-    } catch (_err) {
-      // Ignore non-existent timeout
-    }
-  }
-
-  @OnEvent(CAPACITY_AVAILABLE_EVENT)
-  public async handleCapacityAvailable() {
-    // Avoid spamming the log
-    if (await this.hcpPublishQueue.isPaused()) {
-      this.logger.trace('Capacity Available: Resuming HCP publish queue and clearing timeout');
-    }
-    // Get the failed jobs and check if they failed due to capacity
-    const failedJobs = await this.hcpPublishQueue.getFailed();
-    const capacityFailedJobs = failedJobs.filter((job) =>
-      job.failedReason?.includes('1010: Invalid Transaction: Inability to pay some fees'),
-    );
-    // Retry the failed jobs
-    await Promise.all(
-      capacityFailedJobs.map(async (job) => {
-        this.logger.debug(`Retrying job ${job.id}`);
-        job.retry();
-      }),
-    );
-    try {
-      this.schedulerRegistry.deleteTimeout(CAPACITY_EPOCH_TIMEOUT_NAME);
-    } catch (_err) {
-      // ignore
-    }
-
-    await this.hcpPublishQueue.resume();
   }
 }
--- libs/queue/src/index.ts
+++ libs/queue/src/index.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line no-restricted-exports
 export * from './queue.interfaces';
+export * from './base-publisher.service';
